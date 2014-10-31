9722321e344ff2a395d762ff6388e6b928ee631e
Rony Moura <rony-dev@hotmail.com>
2014-10-31 10:11:15 -0200

    Criação

---
+++ src/TestePlugin.java
@@ -0,0 +1,33 @@
+package com.labofclouds.cordova.testePlugin;
+
+import android.content.Intent;
+import android.net.Uri;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.File;
+
+public class TestePlugin extends CordovaPlugin {
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("doAlert")) {
+            String message = args.getString(0);
+			this.alert(message, callbackContext);
+			
+            return true;
+        }
+		
+        return false;
+    }
+	
+	private void alert(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
---
+++ www/testePlugin.js
@@ -0,0 +1,13 @@
+var testePlugin =  {
+    create: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'TestePlugin', // mapped to our native Java class called "TestePlugin"
+            'doAlert', // with this action name
+            [{"xD"}]
+        );
+    }
+};
+
+module.exports = testePlugin;
